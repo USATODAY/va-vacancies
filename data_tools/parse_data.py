453b6163a8e52da5c78943206d93080a40aeb3e9
mitchthorson <mthorson@usatoday.com>
2015-08-20 13:33:20 -0400

    add full state name to data

--- data_tools/parse_data.py
+++ data_tools/parse_data.py
@@ -1,8 +1,19 @@
+import us
+
 def parse(raw_data):
     entries = raw_data["local_lookup"]
-    entries.append(format_national(raw_data))
-    return entries
+    parsed_entries = []
+    for entry in entries:
+        parsed_entries.append(get_full_state_name(entry))
+    parsed_entries.append(format_national(raw_data))
+    return parsed_entries
 
+def get_full_state_name(entry):
+    new_entry = entry
+    entry_state_abbr = entry["state"]
+    full_state_obj = getattr(us.states, entry_state_abbr)
+    new_entry["full_state"] = full_state_obj.name
+    return new_entry
 
 def format_national(raw_data):
     national_data = raw_data["national_look"][0]
